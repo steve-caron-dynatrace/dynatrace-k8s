8ea6fe058aaec3051937214762380f17b5da8380
steve-caron-dynatrace <steve.caron@dynatrace.com>
2020-01-31 14:15:46 -0500

    hot new service

--- deploy-hot-new-service.sh
+++ deploy-hot-new-service.sh
@@ -5,11 +5,10 @@ NC='\033[0m'
 
 echo -e "{YLW}Deploying a brand hot new service!!!${NC}"
 
-kubectl apply -f manifest/
+kubectl apply -f manifest/hot-new-service/
 
-kubectl -n guestbook create rolebinding default-view --clusterrole=view --serviceaccount=guestbook:default
 
-echo -e "${YLW}Waiting about 30 seconds for all pods to become ready...${NC}"
-sleep 30s
+echo -e "${YLW}Waiting about 10 seconds for all pods to become ready...${NC}"
+sleep 10s
 
-kubectl get po --all-namespaces -l product=guestbook
+kubectl get po
