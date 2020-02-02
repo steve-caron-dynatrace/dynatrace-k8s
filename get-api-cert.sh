7e5ed2c7364d160e6d3588b6113d91d50556d4e6
Steve Caron <steve.caron@dynatrace.com>
2020-02-02 17:56:13 +0000

    scripts and json templates adjustments

--- get-api-cert.sh
+++ get-api-cert.sh
@@ -4,20 +4,14 @@ YLW='\033[1;33m'
 NC='\033[0m'
 
 echo ""    
-read -p "Please enter your Kubernetes API endpoint URL: " API_ENDPOINT_URL
-echo "" 
 
-read -p "API Endpoint URL = ${API_ENDPOINT_URL} . Is this correct? (y/n): " -n 1 -r
+API_ENDPOINT_URL=$(kubectl config view --minify -o jsonpath='{.clusters[0].cluster.server}')
+API_SERVER_PORT="$(echo $API_ENDPOINT_URL | sed -e "s/https:\/\///"):443"
+echo -e "${YLW} API server:${NC} ${API_SERVER_PORT}"
+echo ""
 
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-	API_SERVER_PORT="$(echo $API_ENDPOINT_URL | sed -e "s/https:\/\///"):443"
-	echo $API_SERVER_PORT
-	echo ""
-
-	echo Q | openssl s_client -connect $API_SERVER_PORT 2>/dev/null | openssl x509 -outform PEM > dt_k8s_api.pem
+echo Q | openssl s_client -connect $API_SERVER_PORT 2>/dev/null | openssl x509 -outform PEM > dt_k8s_api.pem
 	
-fi
 
 	
 
