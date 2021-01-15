9d9083609a2c9d8f64d9624983b36feb7dad0487
steve-caron-dynatrace <steve.caron@dynatrace.com>
2021-01-15 13:22:02 -0500

    fixed the istio install script

--- istio/istio-install.sh
+++ istio/istio-install.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 ## downloading istioctl
-curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.7
+curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.7.4 sh -
 
-cd istio-1.7.0
+cd istio-1.7.4
 
-cp values.yaml ./manifests/charts/gateways/istio-ingress/values.yaml
+cp ../values.yaml ./manifests/charts/gateways/istio-ingress/values.yaml
 
 export PATH=$PWD/bin:$PATH
 
 istioctl install --set profile=demo --manifests ./manifests
-
-
