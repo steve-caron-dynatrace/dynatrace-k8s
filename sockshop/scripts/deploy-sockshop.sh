9f481ede33d51a417566d6cd982c0b0a8b9ddd2e
steve-caron-dynatrace <steve.caron@dynatrace.com>
2021-01-16 00:44:34 -0500

    update for the istio scenario

--- sockshop/scripts/deploy-sockshop.sh
+++ sockshop/scripts/deploy-sockshop.sh
@@ -22,6 +22,13 @@ kubectl apply -f ../manifests/backend-services/orders-db/
 kubectl apply -f ../manifests/sockshop-app/sockshop-dev/
 kubectl apply -f ../manifests/sockshop-app/sockshop-production/
 
+if [ ! -z "$1" ] && [ "$1" == "-istio" ]
+  then
+    kubectl apply -f ../manifests/scenarios/istio/gateway.yml
+    kubectl apply -f ../manifests/scenarios/istio/virtual_service_with_carts.yml
+    kubectl apply -f ../manifests/scenarios/istio/destination_rule.yml
+fi
+
 echo -e "${YLW}Waiting about 5 minutes for all pods to become ready...${NC}"
 sleep 330s
 kubectl get po --all-namespaces -l product=sockshop
