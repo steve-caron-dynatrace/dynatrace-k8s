8bc5e6497205e62cfd0cff4dcf51bc9310a6387d
steve-caron-dynatrace <steve.caron@dynatrace.com>
2021-01-16 23:05:05 -0500

    adjusted url for sprint tenants

--- dynatrace/kubernetes/config-cr.sh
+++ dynatrace/kubernetes/config-cr.sh
@@ -15,7 +15,7 @@ ENVIRONMENT_ID=$(grep "DT_ENVIRONMENT_ID=" ../../configuration.conf | sed 's~DT_
 if [ -n "$ENVIRONMENT_ID" ] 
 then
 	echo -e "${YLW}Creating the Custom Resource definition file...${NC}"
-	sed -i "s/ENVIRONMENTID.live.dynatrace.com/$ENVIRONMENT_ID.live.dynatrace.com/" cr.yaml
+	sed -i "s/ENVIRONMENTID.live.dynatrace.com/$ENVIRONMENT_ID.sprint.dynatracelabs.com/" cr.yaml
         echo ""	
 	echo -e "${YLW}Your Operator Custom Resource definition file is: ${NC}cr.yaml"
 else
