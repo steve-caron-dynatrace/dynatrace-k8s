7bbff5cd62498f34ba4f31085cbad31605d87df9
steve-caron-dynatrace <steve.caron@dynatrace.com>
2021-01-17 12:13:55 -0500

    added istio config for the oneagent CR

--- setup.sh
+++ setup.sh
@@ -18,7 +18,7 @@ kubectl apply -f https://github.com/Dynatrace/dynatrace-oneagent-operator/releas
 DT_API_TOKEN=$(grep "DT_API_TOKEN=" ../../configuration.conf | sed 's~DT_API_TOKEN=[ \t]*~~')
 DT_PAAS_TOKEN=$(grep "DT_PAAS_TOKEN=" ../../configuration.conf | sed 's~DT_PAAS_TOKEN=[ \t]*~~')
 kubectl -n dynatrace create secret generic oneagent --from-literal="apiToken="$DT_API_TOKEN --from-literal="paasToken="$DT_PAAS_TOKEN
-./config-cr.sh
+./config-cr.sh -istio
 kubectl apply -f cr.yaml
 sleep 70
 kubectl get po -n dynatrace
