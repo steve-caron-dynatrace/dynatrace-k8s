2c6390939ee852bd712bfed72c3036053ec57cb4
steve-caron-dynatrace <steve.caron@dynatrace.com>
2021-01-25 19:37:17 -0500

    updated

--- hot-bastion-bootstrap.sh
+++ hot-bastion-bootstrap.sh
@@ -16,10 +16,10 @@ else
     fi
 fi
 
-cp -r /home/dtu_training/.kube ~/
+DTU_TRAINING_HOME=/home/dtu_training
 
-git clone https://github.com/steve-caron-dynatrace/dynatrace-k8s.git
-cd dynatrace-k8s
+git clone https://github.com/steve-caron-dynatrace/dynatrace-k8s.git /home/dtu_training/dynatrace-k8s
+cd /home/dtu_training/dynatrace-k8s
 sed -i -r 's~DT_ENVIRONMENT_ID=(.*)~DT_ENVIRONMENT_ID\='"$DYNATRACE_ENVIRONMENT_ID"'~' ./configuration.conf
 sed -i -r 's~DT_API_TOKEN=(.*)~DT_API_TOKEN\='"$DYNATRACE_TOKEN"'~' ./configuration.conf
 sed -i -r 's~DT_PAAS_TOKEN=(.*)~DT_PAAS_TOKEN\='"$DYNATRACE_TOKEN"'~' ./configuration.conf
@@ -29,13 +29,3 @@ CURRENT_DIR=$(pwd)
 
 ## Execute the generic setup script
 ./setup.sh
-
-## Move to /home/dtu_training/
-mv $CURRENT_DIR /home/dtu_training/
-chown -R dtu_training:dtu_training /home/dtu_training/dynatrace-k8s 
-
-# Set persistent env variables
-echo "export DYNATRACE_ENVIRONMENT_ID=$DYNATRACE_ENVIRONMENT_ID" >> /home/dtu_training/.profile
-echo "export DYNATRACE_TOKEN=$DYNATRACE_TOKEN" >> /home/dtu_training/.profileecho "export DYNATRACE_TOKEN=$DYNATRACE_TOKEN" >> /home/dtu_training/.profile
-
-rm -rf ~/.kube
