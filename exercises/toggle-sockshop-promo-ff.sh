820c75fead70183a42f2e5f976e3cdbc393adf45
steve-caron-dynatrace <steve.caron@dynatrace.com>
2021-01-27 02:16:57 -0500

    adjusted script

--- exercises/toggle-sockshop-promo-ff.sh
+++ exercises/toggle-sockshop-promo-ff.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
+  
 YLW='\033[1;33m'
 NC='\033[0m'
 
 echo -e "${YLW}Welcome to Sock Shop Exciting Promo feature flag setting${NC}"
 
 read -p "Do you want to turn on (1) or off (2) the promo feature? Enter 1 or 2 : " -n 1 -r
-
+echo ""
 
 if [[ $REPLY =~ ^[12]$ ]]
 then
@@ -16,10 +16,8 @@ then
     else
             RATE=0
     fi
-    PROD_CARTS_URL=http://$(kubectl describe svc carts -n sockshop-production | grep "LoadBalancer Ingress:" | sed 's/LoadBalancer Ingress:[ \t]*//') 
-    curl "$PROD_CARTS_URL/carts/1/items/promotion/$RATE"
+    CARTS_DEV_URL=$(grep "SOCKSHOP_DEV_CARTS_URL=" ../configuration.conf | sed 's~SOCKSHOP_DEV_CARTS_URL=[ \t]*~~')
+    curl "$CARTS_DEV_URL/carts/1/items/promotion/$RATE"
 else
-echo ""
-echo -e "${YLW}You did not enter a valid option (1 or 2). Nothing will be changed."
+    echo -e "${YLW}You did not enter a valid option (1 or 2). Nothing will be changed."
 fi
-
