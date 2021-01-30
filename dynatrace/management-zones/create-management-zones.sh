0a0c5c1f6d8def865a2015b29a002f6cef23b740
steve-caron-dynatrace <steve.caron@dynatrace.com>
2021-01-30 16:22:33 -0500

    added easytravel monitor to mz

--- dynatrace/management-zones/create-management-zones.sh
+++ dynatrace/management-zones/create-management-zones.sh
@@ -7,7 +7,12 @@ TENANT_ID=$(grep "DT_ENVIRONMENT_ID=" ../../configuration.conf | sed 's~DT_ENVIR
 DT_API_URL="https://$TENANT_ID.sprint.dynatracelabs.com"
 DT_CONFIG_TOKEN=$(grep "DT_CONFIG_TOKEN=" ../../configuration.conf | sed 's~DT_CONFIG_TOKEN=[ \t]*~~')
 
+echo -e "${YLW}Deleting existing Management Zones - 30 seconds${NC}"
+
 ./delete-management-zones.sh
+
+# wait, otherwise there are conflicts between delete and post resulting in constraint violations
+sleep 30s
 
 echo -e "${YLW}Creating Management Zone${NC}"
 RESPONSE=$(curl -X POST "$DT_API_URL/api/config/v1/managementZones" -H "accept: application/json; charset=utf-8" -H "Authorization: Api-Token $DT_CONFIG_TOKEN" -H "Content-Type: application/json; charset=utf-8" -d @mz-app-sockshop.json)
