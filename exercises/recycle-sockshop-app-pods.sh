73a2bc03b6e62f4053918611e8bf94a8484d9afb
steve-caron-dynatrace <steve.caron@dynatrace.com>
2021-01-30 23:03:31 -0500

    added bounce of ingress/egress gateway pods

--- exercises/recycle-sockshop-app-pods.sh
+++ exercises/recycle-sockshop-app-pods.sh
@@ -4,6 +4,8 @@ YLW='\033[1;33m'
 NC='\033[0m'
 
 kubectl delete po --all-namespaces -l product=sockshop
+kubectl delete po -l app=istio-egressgateway -n istio-system
+kubectl delete po -l app=istio-ingressgateway -n istio-system
 
 echo -e "${YLW}Deleting sockshop app pods...${NC}"
 sleep 30s
